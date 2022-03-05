d8abd8d3a8b1ab6d112423ee4ea2408f36a8b802
Corey Oordt <coreyoordt@gmail.com>
2022-03-05 04:56:16 -0600

    Updated command line interface.

--- clgen/cli.py
+++ clgen/cli.py
@@ -1,92 +1,77 @@
 """Command line interface for clgen."""
-from typing import List, Optional
+from typing import Optional
 
 from pathlib import Path
 
 import typer
+from git import Repo
 
-import clgen
-from clgen.configuration import DEFAULT_CONFIG_FILE_NAMES, load_config_file
-from clgen.gitchangelog import changelog, get_log_encoding, get_revision, rest_py
-from clgen.processors.file_processing import stdout
-from clgen.processors.text_processing import noop
-from clgen.utilities import eval_if_callable
+from clgen.configuration import DEFAULT_CONFIG_FILE_NAMES, get_default_config, write_default_config
 
 app = typer.Typer()
 
 
 def version_callback(value: bool):
+    """Display the version and exit."""
+    import clgen
+
     if value:
         typer.echo(clgen.__version__)
         raise typer.Exit()
 
 
+def generate_config_callback(value: bool):
+    """Generate a default configuration file."""
+    if value:
+        f = Path.cwd() / Path(DEFAULT_CONFIG_FILE_NAMES[0])
+        write_default_config(f)
+        typer.echo(f"The configuration file was written to {f}.")
+        raise typer.Exit()
+
+
 @app.command()
 def main(
-    revlist: Optional[List[str]] = typer.Argument(None, help="Optional git-rev-list style arguments"),
     version: Optional[bool] = typer.Option(
-        None, help="show program's version number and exit", callback=version_callback, is_eager=True
+        None, "--version", help="Show program's version number and exit", callback=version_callback, is_eager=True
     ),
-    debug: bool = typer.Option(False, "--debug", "-d", help="Enable debug mode (show full tracebacks)."),
+    generate_config: Optional[bool] = typer.Option(
+        None,
+        "--generate-config",
+        help="Generate a default configuration file",
+        callback=generate_config_callback,
+    ),
     config_file: Optional[Path] = typer.Option(
         None, "--config", "-c", help="Path to the config file.", envvar="CLGEN_CONFIG_FILE"
     ),
 ):
-
-    description_msg = """\
-    Run this command in a git repository to output a formatted changelog.
-    """
-
-    epilog_msg = """\
-    %(exname)s uses a config file to filter meaningful commits or do some
-    formatting in commit messages.
-
-    Config file location will be resolved in this order:
-      - in shell environment variable GITCHANGELOG_CONFIG_FILENAME
-      - in git configuration: ``git config gitchangelog.rc-path``
-      - as '.%(exname)s.rc' in the root of the current git repository
-
-    """
+    """Generate a change  log from git commits."""
+    from clgen import templating
+    from clgen.pipeline import pipeline_factory
 
     # Load default configuration
-    default_config = load_config_file(Path(__file__).parent / "default_config.yaml")
+    config = get_default_config()
 
-    # Load user configuration
-    if config_file:  # was it passed in?
-        user_config = load_config_file(config_file)
-    else:  # Is there a configuration file?
-        user_config = next(
-            (load_config_file(Path(name)) for name in DEFAULT_CONFIG_FILE_NAMES if Path(name).exists()),
-            {},
-        )
+    if user_config := config_file or next(
+        (Path.cwd() / Path(name) for name in DEFAULT_CONFIG_FILE_NAMES if (Path.cwd() / Path(name)).exists()), None
+    ):
+        typer.echo(f"Using configuration file: {user_config}")
+        config.update_from_file(user_config)
+    else:
+        typer.echo("No configuration file found. Using default configuration.")
 
-    default_config.update(user_config)
+    repository = Repo(search_parent_directories=True)
 
-    # get git repo
-    repository = ...
+    # get starting tag based on configuration
+    start_tag_pipeline = pipeline_factory(config.starting_tag_pipeline)
+    starting_tag = start_tag_pipeline.run()
+    if not starting_tag:
+        typer.echo("No starting tag found. Generating entire change log.")
+    else:
+        typer.echo(f"Generating change log from tag: '{starting_tag}'.")
 
-    log_encoding = get_log_encoding(repository, config)
-    revlist = get_revision(repository, config, opts)
-    config["unreleased_version_label"] = eval_if_callable(config["unreleased_version_label"])
-
-    content = changelog(
-        repository=repository,
-        revlist=revlist,
-        ignore_regexps=config["ignore_regexps"],
-        section_regexps=config["section_regexps"],
-        unreleased_version_label=config["unreleased_version_label"],
-        tag_filter_regexp=config["tag_filter_regexp"],
-        output_engine=config.get("output_engine", rest_py),
-        include_merge=config.get("include_merge", True),
-        body_process=config.get("body_process", noop),
-        subject_process=config.get("subject_process", noop),
-        log_encoding=log_encoding,
-    )
-
-    if isinstance(content, str):
-        content = content.splitlines(True)
-
-    config.get("publish", stdout)(content)
+    # use the output pipeline to deal with the rendered change log.
+    output_pipeline = pipeline_factory(config.output_pipeline)
+    output_pipeline.run(templating.render(repository, config, starting_tag))
 
 
 if __name__ == "__main__":
